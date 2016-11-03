4732f73cb07a7404b3e8613eff046da3f63f6678
Isuru <github email address>
2016-11-03 11:11:04 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -12,3 +12,10 @@ function updateIngredient(recipe, item, size){
   recipe[item] = size;
   recipe;
 }
+
+function readRecipe(recipe){
+  var i = 0;
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " I ate " + key );
+  }
+}
