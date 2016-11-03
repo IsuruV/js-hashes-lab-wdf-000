06c0c2219a6280eb7ad13bc0075d6dc099e9137e
Isuru <github email address>
2016-11-03 10:51:57 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function addIngredient(recipe, item, size){
+  recipe[item] = size;
+}
+
+function removeIngredient(recipe, item){
+  delete recipe[item];
+}
+
+function updateIngredient(recipe, item, size){
+  recipe[item] = size;
+  recipe;
+}
